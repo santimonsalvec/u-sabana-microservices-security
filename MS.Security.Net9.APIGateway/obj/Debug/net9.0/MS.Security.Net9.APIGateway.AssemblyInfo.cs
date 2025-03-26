60675f27fa9c32b15844ef9752fe7fa298cf9a25
santimonsalvec <santimonsalvec@gmail.com>
2025-03-26 01:29:52 -0500

    test configurations

--- MS.Security.Net9.APIGateway/obj/Debug/net9.0/MS.Security.Net9.APIGateway.AssemblyInfo.cs
+++ MS.Security.Net9.APIGateway/obj/Debug/net9.0/MS.Security.Net9.APIGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MS.Security.Net9.APIGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3133eed693c70a57462b3321905876c0ba07a190")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c3d02491cd2099cdfe6791c7ad1d5b3e98919a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MS.Security.Net9.APIGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MS.Security.Net9.APIGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
